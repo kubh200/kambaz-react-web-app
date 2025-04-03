cba7da845502859c4e33fb291d32372c4b581c49
Duxbeh <zijiechen122@gmail.com>
2025-04-03 02:13:10 -0400

    a5 changess

--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -57,11 +57,12 @@ export default function Kambaz() {
 
   const deleteCourse = async (courseId: string) => {
     const status = await courseClient.deleteCourse(courseId);
-    if (status === 200) {
-      setCourses(courses.filter((course) => course._id !== courseId));
-    } else {
-      console.error("❌ Failed to delete course:", status);
-    }
+    // if (status === 200) {
+    //   setCourses(courses.filter((course) => course._id !== courseId));
+    // } else {
+    //   console.error("❌ Failed to delete course:", status);
+    // }
+    setCourses(courses.filter((course) => course._id !== courseId));
   };
 
 
